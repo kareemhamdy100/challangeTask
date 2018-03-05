6de6433fadef2373a0a0af32538d53a8b103f343
David Guttman <david@davidguttman.com>
2018-03-05 08:56:36 -0800

    feat: add echo route

--- test/routes.js
+++ test/routes.js
@@ -84,7 +84,6 @@ tape('should get stream', function (t) {
     })
 })
 
-tape('should get reverse', function (t) {
   var expected = {
     input: 'stringtoreverse',
     output: 'esreverotgnirts'
@@ -93,9 +92,17 @@ tape('should get reverse', function (t) {
   var url = '/reverse/' + expected.input
   servertest(server(), url, {encoding: 'json'}, function (err, res) {
     t.ifError(err, 'no error')
-
     t.equal(res.statusCode, 200, 'correct statusCode')
     t.deepEqual(res.body, expected, 'values should match')
     t.end()
   })
+tape('should get echo', function (t) {
+
+servertest(server(), url, {encoding: 'json'}, function (err, res) {
+  t.ifError(err, 'no error')
+  t.equal(res.statusCode, 200, 'correct statusCode')
+  t.deepEqual(res.body, {one: '1', two: '2'}, 'values should match')
+  t.end()
+})
+ 
 })
